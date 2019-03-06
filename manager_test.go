e023fa851c27c99cda99e12fe4ec5829ff6b34dc
Anton Mashko <anton.mashko@gmail.com>
2019-03-07 01:50:17 +0200

    added simple test for task manager

--- manager_test.go
+++ manager_test.go
@@ -1,1 +1,22 @@
-package taskq_test
+package taskq
+
+import (
+	"testing"
+)
+
+func TestTaskFromTaskMenegerOk(t *testing.T) {
+	m := NewTaskManger(New(1))
+	block := make(chan struct{})
+	task := TaskFunc(func() error {
+		<-block
+		return nil
+	})
+	id := m.Enqueue(task)
+	m.Start()
+	mtask := m.Task(id)
+	if mtask == nil {
+		t.Error("task not found")
+	}
+	block <- struct{}{}
+	m.Close()
+}
