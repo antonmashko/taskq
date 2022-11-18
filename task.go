2803e72d2a1ed8a676013f228a7417f9f2bedf94
Anton Mashko <anton.mashko@gmail.com>
2022-11-18 15:48:36 +0200

    removed retry task

--- task.go
+++ task.go
@@ -2,10 +2,7 @@ package taskq
 
 import (
 	"context"
-	"errors"
 )
-
-var ErrRetryTask = errors.New("error occurred during task execution. retry task")
 
 // Task for TaskQ
 type Task interface {
@@ -24,29 +21,6 @@ type TaskFunc func(ctx context.Context) error
 
 func (t TaskFunc) Do(ctx context.Context) error {
 	return t(ctx)
-}
-
-type RetryableTask struct {
-	task       Task
-	maxRetries int
-}
-
-func NewRetryableTask(task Task, maxRetries int) Task {
-	return &RetryableTask{task: task, maxRetries: maxRetries}
-}
-
-func (t *RetryableTask) Do(ctx context.Context) error {
-	for i := 0; i < t.maxRetries; i++ {
-		err := t.task.Do(context.Background())
-		if err == nil {
-			return nil
-		}
-		if err == ErrRetryTask {
-			continue
-		}
-		return err
-	}
-	return nil
 }
 
 func processTask(ctx context.Context, task Task) {
